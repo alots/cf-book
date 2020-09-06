aa53cc318be89842f00c718c0499592cdf975786
alots <alexeylots@gmail.com>
2020-09-06 14:58:18 +0400

    clear style

--- src/components/BookPreview/BookPreviewContainer.js
+++ src/components/BookPreview/BookPreviewContainer.js
@@ -13,14 +13,13 @@ import SimilarBookList from "./SimilarBookList";
 import PriceForm from "./PriceForm";
 import withLoader from "../../HOC/withLoader";
 import withBookInformation from "../../HOC/withBookInformation";
-import Btn from "../ui/Btn"
+import Btn from "../ui/Btn";
 import withBtnUp from "../../HOC/withBtnUp";
 
-const BtnUp = withBtnUp(Btn,settings.visibleFrom);
+const BtnUp = withBtnUp(Btn, settings.visibleFrom);
 
 const BookPreviewContainer = ({ book }) => {
   const [countSubscribers, setCountSubscribers] = useState(0);
-  
 
   const handleClick = () => {
     setCountSubscribers(countSubscribers + 1);
@@ -28,7 +27,7 @@ const BookPreviewContainer = ({ book }) => {
 
   return (
     <Container>
-      <Row className="row-1">
+      <Row className="bookPreview-row">
         <Col sm="12" md="4" className="author-card">
           <AuthorСard
             authors={book.authors}
